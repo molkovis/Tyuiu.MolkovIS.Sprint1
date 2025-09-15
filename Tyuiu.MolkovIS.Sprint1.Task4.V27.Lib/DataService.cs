fa73fa3e68f08b0af009e27770d7a9ebc2a10bab
MolkovIS <Tyuiu.MolkovIS@outlook.com>
2025-09-15 19:35:06 +0500

    Task4

--- Tyuiu.MolkovIS.Sprint1.Task4.V27.Lib/DataService.cs
+++ Tyuiu.MolkovIS.Sprint1.Task4.V27.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.MolkovIS.Sprint1.Task4.V27.Lib
     {
         public double Calculate(double x, double y)
         {
-            return(Math.Round(((1+Math.Sin(Math.PI*x))/(x-Math.Sqrt(Math.Abs(y)))),5));
+            return(Math.Round(((1+Math.Sin(Math.PI*x))/(x-Math.Sqrt(Math.Abs(y)))),3));
         }
     }
 }
