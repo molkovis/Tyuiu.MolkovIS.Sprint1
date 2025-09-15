918e0b7270160430af615c1ef2af84b0cdab29e0
MolkovIS <Tyuiu.MolkovIS@outlook.com>
2025-09-15 18:31:25 +0500

    Task5

---
+++ Tyuiu.MolkovIS.Sprint1.Task5.V27.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.MolkovIS.Sprint1.Task5.V27.Lib
+{
+    public class DataService : ISprint1Task5V3
+    {
+        public int Calculate(int k)
+        {
+            int h;
+            string numString = Convert.ToString(k);
+            string a = Convert.ToString(numString[numString.Length - 3]);
+            h = Convert.ToInt32(a);
+            return h;
+        }
+    }
+}
---
+++ Tyuiu.MolkovIS.Sprint1.Task5.V27/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.MolkovIS.Sprint1.Task5.V27.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Title = "Спринт #1 | Выполнил: Мольков И. С.| ПКТб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт#01                                                               *");
+        Console.WriteLine("* Тема: Преобразование типов и класс Convert                              *");
+        Console.WriteLine("* Задание #5                                                              *");
+        Console.WriteLine("* Вариант #3                                                              *");
+        Console.WriteLine("* Выполнил: Мольков Иван Сергеевич                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Условие:                                                                *");
+        Console.WriteLine("* Написать программу, которая решает следующую задачу:                    *");
+        Console.WriteLine("* Присвоить целой переменной h третью от конца цифру                      *");
+        Console.WriteLine("* в записи положительного целого числа k,                                 *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         ");
+        Console.WriteLine("***************************************************************************");
+
+
+        Console.Write("Введите положительное целое число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        DataService ds = new DataService();
+
+
+
+
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        int res = ds.Calculate(number);
+        Console.WriteLine(res);
+        Console.ReadKey();
+    }
+}
